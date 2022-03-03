14a5c4f876280d37c9896eb1089f6e2bedec88e8
Herman Cai <hermancai3@gmail.com>
2022-03-03 04:17:39 -0800

    Write unit tests for model class

--- lib/models/single_post.dart
+++ lib/models/single_post.dart
@@ -4,4 +4,14 @@ class SinglePost {
   int? quantity;
   double? latitude;
   double? longitude;
+
+  SinglePost();
+
+  SinglePost.fromMap(Map map) {
+    date = map["date"];
+    imageURL = map["imageURL"];
+    quantity = map["quantity"];
+    latitude = map["latitude"];
+    longitude = map["longitude"];
+  }
 }
---
+++ test/model_test.dart
@@ -0,0 +1,47 @@
+import 'package:test/test.dart';
+import 'package:wasteagram/models/single_post.dart';
+
+void main() {
+  test("Post built from map should have appropriate values", () {
+    final date = DateTime.parse("2022-02-02");
+    const imageURL = "url";
+    const quantity = 5;
+    const latitude = 1.2;
+    const longitude = 3.4;
+
+    final post = SinglePost.fromMap({
+      "date": date,
+      "imageURL": imageURL, 
+      "quantity": quantity,
+      "latitude": latitude,
+      "longitude": longitude
+    });
+
+    expect(post.date, date);
+    expect(post.imageURL, imageURL);
+    expect(post.quantity, quantity);
+    expect(post.latitude, latitude);
+    expect(post.longitude, longitude);
+  });
+
+  test("Post built with manual setters should have appropriate values", () {
+    final date = DateTime.parse("2022-02-02");
+    const imageURL = "url";
+    const quantity = 5;
+    const latitude = 1.2;
+    const longitude = 3.4;
+
+    final post = SinglePost();
+    post.date = date;
+    post.imageURL = imageURL;
+    post.quantity = quantity;
+    post.latitude = latitude;
+    post.longitude = longitude;
+
+    expect(post.date, date);
+    expect(post.imageURL, imageURL);
+    expect(post.quantity, quantity);
+    expect(post.latitude, latitude);
+    expect(post.longitude, longitude);
+  });
+}
